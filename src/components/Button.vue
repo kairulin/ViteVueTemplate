c8f52d01d6ff59f641b7be69f33a4ac6b2e7c702
LinKarRu <wl03352521@gmail.com>
2024-06-11 14:22:46 +0800

    cva buttom

---
+++ src/components/Button.vue
@@ -0,0 +1,58 @@
+<template>
+    <component :is="as" :disalbed="disabled" :class="buttonClass">
+        <svg v-if="loading" class="animate-spin h-5 w-5 absolute" xmlns="http://www.w3.org/2000/svg" fill="none"
+            viewBox="0 0 24 24">
+            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+            <path class="opacity-75" fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
+            </path>
+        </svg>
+        <component :is="leftIcon" v-if="!loading" :class="['w-5 h-5 mr-2', loading && 'invisible']" />
+        <span :class="[loading && 'invisible']">
+            <slot />
+        </span>
+        <component :is="rightIcon" v-if="!loading" :class="['w-5 h-5 ml-2', loading && 'invisible']" />
+    </component>
+</template>
+<script setup lang="ts">
+import { cva } from 'class-variance-authority';
+import { computed } from 'vue';
+const props = defineProps({
+    leftIcon: Object,
+    rightIcon: Object,
+    loading: Boolean,
+    disabled: Boolean,
+    as: {
+        type: [String, Object],
+        default: 'button'
+    },
+    intent: {
+        type: String,
+        validator: (val: string) => ['primary', 'secondary', 'danger'].includes(val),
+        default: 'secondary'
+    }
+});
+
+const buttonClass = computed(() => {
+    const intentValue = props.intent as "primary" | "secondary" | "danger";
+    return cva("inline-flex items-center justify-center text-sm rounded min-h-[32px] px-3 py-0.5 font-semibold ",
+        {
+            variants: {
+                intent: {
+                    primary: "bg-black text-white hover:bg-gray-800",
+                    secondary: "bg-black/5 hover:bg-black/10",
+                    danger: "bg-red-600 text-white hover:bg-red-500",
+                    text: "text-gray-700 hover:bg-black/5"
+                },
+                disabled: {
+                    true: "!bg-gray-100 !text-gray-400 cursor-not-allowed"
+                }
+            }
+        }
+    )(
+        {
+            intent: intentValue,
+            disabled: props.disabled,
+        });
+})
+</script>
---
+++ src/views/ButtonPage.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="ml-5 mt-5 space-y-5">
+        <div class="flex gap-5 flex-wrap">
+            <Button as="a" href="http://localhost:5174/" intent="primary">Primary</Button>
+            <Button as="a" href="http://localhost:5174/">Secondary</Button>
+            <Button as="a" href="http://localhost:5174/" intent="danger">Danger</Button>
+            <Button as="a" href="http://localhost:5174/" intent="text">PrimaTextry</Button>
+        </div>
+        <div class="flex gap-5 flex-wrap">
+            <Button as="a" href="http://localhost:5174/" loading intent="primary"
+                :left-icon="EnvelopeIcon">Primary</Button>
+            <Button as="a" href="http://localhost:5174/" loading :left-icon="EnvelopeIcon">Secondary</Button>
+            <Button as="a" href="http://localhost:5174/" loading intent="danger"
+                :left-icon="EnvelopeIcon">Danger</Button>
+            <Button as="a" href="http://localhost:5174/" loading intent="text"
+                :left-icon="EnvelopeIcon">PrimaTextry</Button>
+        </div>
+        <div class="flex gap-5 flex-wrap">
+            <Button as="a" href="http://localhost:5174/" intent="primary" :left-icon="EnvelopeIcon">Primary</Button>
+            <Button as="a" href="http://localhost:5174/" :left-icon="EnvelopeIcon">Secondary</Button>
+            <Button as="a" href="http://localhost:5174/" intent="danger" :left-icon="EnvelopeIcon">Danger</Button>
+            <Button as="a" href="http://localhost:5174/" intent="text" :left-icon="EnvelopeIcon">PrimaTextry</Button>
+        </div>
+        <div class="flex gap-5 flex-wrap">
+            <Button as="a" href="http://localhost:5174/" intent="primary" :right-icon="EnvelopeIcon">Primary</Button>
+            <Button as="a" href="http://localhost:5174/" :right-icon="EnvelopeIcon">Secondary</Button>
+            <Button as="a" href="http://localhost:5174/" intent="danger" :right-icon="EnvelopeIcon">Danger</Button>
+            <Button as="a" href="http://localhost:5174/" intent="text" :right-icon="EnvelopeIcon">PrimaTextry</Button>
+        </div>
+        <div class="flex gap-5 flex-wrap">
+            <Button as="a" href="http://localhost:5174/" disabled intent="primary"
+                :left-icon="EnvelopeIcon">Primary</Button>
+            <Button as="a" href="http://localhost:5174/" disabled :left-icon="EnvelopeIcon">Secondary</Button>
+            <Button as="a" href="http://localhost:5174/" disabled intent="danger"
+                :left-icon="EnvelopeIcon">Danger</Button>
+            <Button as="a" href="http://localhost:5174/" disabled intent="text"
+                :left-icon="EnvelopeIcon">PrimaTextry</Button>
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+import { EnvelopeIcon } from '@heroicons/vue/20/solid';
+import Button from '@/components/Button.vue'
+</script>
